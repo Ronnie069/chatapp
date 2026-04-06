b460e3a314ffe8976e68604d8e9691a1f35698cf
Ronald <rkmukanya27@gmail.com>
2026-04-06 17:52:23 +0200

    feat: initialize Login class with user fields and constructor

---
+++ src/main/java/com/chatapp/Login.java
@@ -0,0 +1,16 @@
+package com.chatapp;
+
+public class Login {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    // Just the constructor for now
+    public Login(String username, String password, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
