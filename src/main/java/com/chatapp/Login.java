2afce61968688a4af424816d23b40e9838dfb85a
Ronald <rkmukanya27@gmail.com>
2026-04-06 18:02:31 +0200

    feat: implement checkPasswordComplexity using regex

--- src/main/java/com/chatapp/Login.java
+++ src/main/java/com/chatapp/Login.java
@@ -7,7 +7,6 @@ public class Login {
     private String firstName;
     private String lastName;
 
-    // Just the constructor for now
     public Login(String username, String password, String firstName, String lastName) {
         this.username = username;
         this.password = password;
@@ -18,4 +17,14 @@ public class Login {
     public boolean checkUserName() {
         return username.contains("_") && username.length() <= 5;
     }
+
+    public boolean checkPasswordComplexity() {
+    // Regex explanation: 
+    // (?=.*[A-Z]) -> at least one uppercase
+    // (?=.*[0-9]) -> at least one digit
+    // (?=.*[!@#$%^&*]) -> at least one special char
+    // .{8,} -> at least 8 characters long
+    String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$";
+    return java.util.regex.Pattern.compile(regex).matcher(this.password).matches();
+}
 }
